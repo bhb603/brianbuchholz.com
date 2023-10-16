f3a9ee12db7255e0a1f0fcf60b1809214fef8ec6
Brian H. Buchholz <4773480+bhb603@users.noreply.github.com>
2023-10-16 10:12:18 -0400

    initial setup

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://brianbuchholz.com',
+	integrations: [mdx(), sitemap()],
+	markdown: {
+		shikiConfig: {
+			theme: 'rose-pine-dawn',
+		},
+	}
+});
