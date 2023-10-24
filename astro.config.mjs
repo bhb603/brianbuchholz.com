65f26d93f79bc96673ce21870dc8e46cc5355ec6
Brian H. Buchholz <4773480+bhb603@users.noreply.github.com>
2023-10-24 19:54:44 -0400

    setup gh actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://brianbuchholz.com',
+	site: 'https://www.brianbuchholz.com',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		shikiConfig: {
